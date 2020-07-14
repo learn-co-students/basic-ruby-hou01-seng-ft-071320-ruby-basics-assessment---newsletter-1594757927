663a61ec3713ff2b835b3c5b4adeaa1711645f32
yordy130 <yordinrodriguez@rocketmail.com>
2020-07-14 16:59:19 -0500

    Basic Ruby

--- newsletter.rb
+++ newsletter.rb
@@ -30,10 +30,12 @@ ARTICLES = [
 def calculate_recipients
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
+  puts "hello"
+  binding.pry
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+def first_n_articles(number_of_articles)
+  ARTICLES.first 
 end
 
 def print_recipients
@@ -54,7 +56,8 @@ def print_many_articles(articles)
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{campus[:name]}"
+  #binding.pry
 end
 
 def format_subject
@@ -80,7 +83,6 @@ def print_newsletter(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
 end
 
 def run
